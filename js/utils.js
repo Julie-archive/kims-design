6d647a45c466bbe0b8aac8fad3b71d0e4dec4345
Julie-archive <smiledbfgml@gmail.com>
2026-03-31 10:09:28 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,14 @@ function formatTelInput(el) {
   else if(v.length <= 7) el.value = v.slice(0,3) + '-' + v.slice(3);
   else el.value = v.slice(0,3) + '-' + v.slice(3,7) + '-' + v.slice(7,11);
 }
+
+// 입력값의 HTML 태그를 무력화하여 XSS 공격을 방어하는 유틸리티 함수
+function escapeHTML(str) {
+  if (!str) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
